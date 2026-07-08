5dbb91c0ccb00be8512cd1f4d4f4550d7aa201fe
Arshia <ahmadreza.azadian@gmail.com>
2026-07-08 14:12:08 +0200

    Fixed CI/CD failing tests

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -27,21 +27,20 @@ class ProductFactory extends Factory
             'description' => fake()->optional()->sentence(),
             'category_id' => null,
             'quantity' => $quantity,
-            'available_quantity' => $quantity,
             'is_active' => true,
         ];
     }
 
     public function configure()
     {
-        return $this->afterCreating(function (Product $product) {
-            $imageId = fake()->numberBetween(1, 1000);
-            try {
-                // To avoid long seeding times and test failures, you can mock or skip downloading
+        return $this
+            ->afterMaking(function (Product $product) {
+                // Sync available_quantity with quantity when not explicitly provided.
+                $product->available_quantity = $product->available_quantity ?? $product->quantity;
+            })
+            ->afterCreating(function (Product $product) {
+                // To avoid long seeding times and test failures, we can mock or skip downloading
                 // $product->addMediaFromUrl("https://picsum.photos/640/480?random={$imageId}")->toMediaCollection('photo');
-            } catch (\Exception $e) {
-                // Ignore download errors on seeding
-            }
-        });
+            });
     }
 }
